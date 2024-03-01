b12dc124c64ce22d1e12ddbaf32f2117122c9f91
PicoC-GIT <137415363+PicoC-GIT@users.noreply.github.com>
2024-03-01 13:13:29 -0600

    Avances completos (Intento2)

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
+
+    <title>ALFA Escuela Profesional de Manejo</title>
+</head>
+<body>
+	<?php include_once("PhpAssets/header.php"); ?>
+    <div class="FIRST-container">
+        <div id="imagen-container"></div>
+        <div id="bloc">
+            <h1 class="TitulosG">4C</h1>
+            <h2 id="TextoS">Centro de Capacitación Certificado en Conducción</h2>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
