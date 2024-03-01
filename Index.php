259b6654e89e214b8bf27531e106db7669aa9ae9
PicoC-GIT <137415363+PicoC-GIT@users.noreply.github.com>
2024-03-01 15:25:06 -0600

    Agregados

--- Index.php
+++ Index.php
@@ -15,10 +15,15 @@
         <div id="bloc">
             <h1 class="TitulosG">4C</h1>
             <h2 id="TextoS">Centro de Capacitación Certificado en Conducción</h2>
+            <div id="myButtonContainer">
+                <button id="myButton">Quiero certificarme</button>
+            </div>
         </div>
-
     </div>
-
+    <div id="BlueBloc">
+        <div id="TituloG2">QUIENES SOMOS</div>
+        <div id="TextoXS">Somos un grupo de profesionales en la cultura vial con 30 años de experiencia, que preocupados ante la alarmante incidencia de accidentes vehiculares por la falta de capacitación se unieron, para crear un Centro de Capacitación en Conducción con la finalidad de educar a las próximas generaciones de conductores, para que se protejan así mismos, a su familia y al resto de las personas que utilizan las vías públicas.</div>
+    </div>
 
 </body>
 </html>
