56b37ed09a311673e7c0215079c90ef5dc005bca
PicoC-GIT <137415363+PicoC-GIT@users.noreply.github.com>
2024-03-06 17:54:46 -0600

    Footer contacto

--- contacto.php
+++ contacto.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <script type="text/javascript" src="script.js"></script>
-    <title>ALFA Escuela Profesional de Manejo</title>
+    <title>Contacto</title>
 </head>
 <body>
 <?php include_once("PhpAssets/header.php"); ?>
