6adb9fda0745990e6234c31d3f1db58d8b6bf8dd
PicoC-GIT <137415363+PicoC-GIT@users.noreply.github.com>
2024-03-07 17:21:46 -0600

    Index base final (no conectado)

--- Index.php
+++ Index.php
@@ -20,7 +20,7 @@
             </div>
         </div>
     </div>
-    <div id="BlueBloc">
+    <div class="BlueBloc">
         <div style="display: block; height: 0;"></div>
         <div id="TituloG2">Q&nbsp;U&nbsp;I&nbsp;E&nbsp;N&nbsp;E&nbsp;S&nbsp; &nbsp;S&nbsp;O&nbsp;M&nbsp;O&nbsp;S</div>
         <div id="TextoXS">Somos un grupo de profesionales en la cultura vial con 30 años de experiencia, que preocupados ante la alarmante incidencia de accidentes vehiculares por la falta de capacitación se unieron, para crear un Centro de Capacitación en Conducción con la finalidad de educar a las próximas generaciones de conductores, para que se protejan así mismos, a su familia y al resto de las personas que utilizan las vías públicas.</div>
@@ -28,9 +28,22 @@
     <div id="BlueTxt">S&nbsp;E&nbsp;R&nbsp;V&nbsp;I&nbsp;C&nbsp;I&nbsp;O&nbsp;S</div>
     <div id="NegroTxt">Servicios Educativos impartidos por el sector privado.</div>
     <div id="SECOND-container">
-        <div class="OrangBloc"></div>
-        <div class="OrangBloc"></div>
-        <div class="OrangBloc"></div>
+        <div class="OrangBloc"><img class="Logos" src="CarLogo.svg" alt="Car"><div class="Txtbloc">Curso de manejo para principiantes</div></div>
+        <div class="OrangBloc"><img class="Logos" src="LicenciaLogo.svg" alt="Licencia"><div class="Txtbloc">Curso de manejo intermedio</div></div>
+        <div class="OrangBloc"><img class="Logos" src="RayLogo.svg" alt="Rayo"><div class="Txtbloc">Curso de manejo masivo</div></div>
+    </div>
+    <div id="myButtonContainer2">
+        <button id="myButton2">Contactanos</button>
+    </div>
+    <div class="BlueBloc">
+        <div id="TxtCirc">Adquiere el conocimiento suficiente para conducir y obtener su certificado con validez oficial que lo acredite.</div>
+        <div><img id="circulito" src="OrangeCircle.svg" alt="Circulo"></div>
+    </div>
+    <div><img id="Real" src="CarroReal.svg" alt="Carro"></div>
+    <div id="THIRD-container">
+        <div class="CircularBloc"><div class="TxtOrg">MISIÓN</div><div class="SubTxt">Educar para prevenir</div></div>
+        <div class="CircularBloc"><div class="TxtOrg">VISIÓN</div><div class="SubTxt">Crear una nueva cultura de respeto al conducir</div></div>
+        <div class="CircularBloc"><div class="TxtOrg">VALORES</div><div class="FlexText"><span class="word">Confianza</span><span class="word">Respeto</span><span class="word">Equidad</span><span class="word">Empatía</span></div></div>
     </div>
     <?php include_once("PhpAssets/footer.php"); ?>
 </body>
